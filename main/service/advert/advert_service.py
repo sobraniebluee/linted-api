c33a9bbd88faedb6d650e484bd208fd44d067bf8
sobraniebluee <dimf29@gmail.com>
2022-06-14 01:16:01 +0200

    I forget

--- main/service/advert/advert_service.py
+++ main/service/advert/advert_service.py
@@ -5,13 +5,71 @@ from main.models.Category.category_model import Category
 from main.models.Size.size_model import Size
 from main.middleware.error import Error
 from sqlalchemy import or_
+from config import Config
 
 
-def get_adverts_service():
-    adverts = Advert.query.all()
-    if not adverts:
-        return Error.error_not_found()
-    return adverts, 200
+def get_adverts_service(**kwargs):
+    categories_id = kwargs.get('categories_id', None)
+    conditions_id = kwargs.get('conditions_id', None)
+    sizes_id = kwargs.get('sizes_id', None)
+    search_text = kwargs.get('search_text', None)
+    sort = kwargs.get('sort', None)
+    price_from = kwargs.get('price_from', 0)
+    price_to = kwargs.get('price_to', Config.MAX_PRICE)
+    page = kwargs.get('page', 1)
+
+    if sort == 'desc':
+        order_sort = AdvertInfo.price.desc()
+    elif sort == 'asc':
+        order_sort = AdvertInfo.price.asc()
+    elif sort == 'newest':
+        order_sort = Advert.created_at.asc()
+    else:
+        order_sort = Advert.id.desc()
+
+    stm = session.query(Advert)
+    if categories_id:
+        found_all_sub_catalogs(categories_id)
+        stm = stm.where(AdvertInfo.category_id.in_(categories_id))
+    if sizes_id:
+        stm = stm.where(AdvertInfo.size_id.in_(sizes_id))
+    if conditions_id:
+        stm = stm.where(AdvertInfo.condition_id.in_(conditions_id))
+
+    stm = stm.filter(
+        AdvertInfo.price >= price_from,
+        AdvertInfo.price <= price_to,
+        AdvertInfo.id_advert == Advert.id
+    )
+    stm_cte = stm.cte()
+    adverts_id = session.query(stm_cte).all()
+    if adverts_id:
+        tmp_id = []
+        for id_advert, *args in adverts_id:
+            tmp_id.append(id_advert)
+        adverts = Advert.query.where(
+                Advert.id.in_(tmp_id)).filter(
+                AdvertInfo.id_advert == Advert.id
+            ).order_by(order_sort).offset(
+            page * Config.PAGE_COUNT_ADVERT
+        ).limit(
+            Config.PAGE_COUNT_ADVERT
+        ).all()
+    else:
+        adverts = []
+
+    page = page + 1 if page == 0 else page
+    adverts_count = len(adverts_id)
+    total_pages = adverts_count / Config.PAGE_COUNT_ADVERT
+    response = {
+        'items': adverts,
+        'pagination': {
+            'current_page': page,
+            'total_entries': adverts_count,
+            'total_pages': total_pages
+        }
+    }
+    return response, 200
 
 
 def get_advert_by_url_service(identity, url_advert):
@@ -20,6 +78,14 @@ def get_advert_by_url_service(identity, url_advert):
         return Error.error_not_found()
     watch_advert(identity=identity, id_advert=advert.id)
     return advert, 200
+
+
+def found_all_sub_catalogs(categories_id):
+    for category_id in categories_id:
+        print(category_id)
+        category = Category.query.filter(Category.id_category == category_id).first()
+        if not category:
+            print(category.id_category)
 
 
 def add_new_advert_data_service(id_user, title, description, price, category_id, condition_id, images, size_id=None):
@@ -127,6 +193,7 @@ def check_size_id(category, size_id):
         return True
 
 
+# Нужно для проверки категории
 def recursive_find_root_category(id_root):
     root_category = Category.query.filter(Category.id_category == id_root).first()
     if root_category.id_root == 1:
@@ -152,4 +219,5 @@ def watch_advert(identity, id_advert):
         if identity['jwt'] and is_watch.id_user is None:
             setattr(is_watch, 'id_user', identity['jwt'])
             session.commit()
+
 
