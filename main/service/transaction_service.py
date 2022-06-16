04f9797636ed4d8a774d7772e9e30ebb99abc500
sobraniebluee <dimf29@gmail.com>
2022-06-16 03:25:48 +0200

    Optimize transaction service

--- main/service/transaction_service.py
+++ main/service/transaction_service.py
@@ -9,31 +9,38 @@ def create_transaction_service(id_user, id_advert):
     advert = Advert.query.filter(Advert.id == id_advert, Advert.id_user != id_user).first()
     if not advert:
         return Error.server_error()
+
     if advert.is_bought:
         return Error.error_default(msg="Product already bought", status_code=400)
-    buyer = User.query.filter(User.id == id_user).first()
-    seller = User.query.filter(User.id == advert.id_user).first()
+    buyer_id, buyer_balance = session.query(User.id, User.balance).filter(User.id == id_user).first()
+    seller_id, = session.query(User.id).filter(User.id == advert.id_user).first()
 
-    if not buyer or not seller:
+    if not buyer_id or not seller_id:
         return Error.server_error()
 
     offer = session.query(Offer.price).filter(Offer.id_advert == advert.id,
-                                              Offer.id_buyer == id_user,
+                                              Offer.id_buyer == buyer_id,
                                               Offer.is_accepted == True).first()
     if offer:
-        price = offer.price
+        # got price from tuple
+        price, = offer
     else:
         price = advert.info.price
 
-    if buyer.balance < price:
+    if buyer_balance < price:
         return Error.error_default(msg="Sorry,please top up your balance!", status_code=400)
-
     try:
-        new_transaction = Transaction(buyer.id, seller.id, advert.id, price)
-        new_transaction.save()
+        transaction = Transaction.query.filter(Transaction.id_buyer == buyer_id,
+                                               Transaction.id_advert == advert.id).first()
+        if transaction:
+            setattr(transaction, 'price', price)
+            transaction.commit()
+        else:
+            transaction = Transaction(buyer_id, seller_id, advert.id, price)
+            transaction.save()
     except Exception:
         return Error.server_error(msg="Something went wrong")
-    return new_transaction, 200
+    return transaction, 200
 
 
 def complete_transaction_service(id_transaction, id_buyer):
@@ -71,30 +78,3 @@ def complete_transaction_service(id_transaction, id_buyer):
         session.rollback()
         return Error.server_error()
 
-# def advert_purchase_service(id_user, url_advert):
-#     advert = Advert.query.filter(Advert.url == url_advert).first()
-#     if not advert:
-#         Error.server_error()
-#     buyer = User.query.filter(User.id == id_user).first()
-#     seller = User.query.filter(User.id == advert.id_user).first()
-#
-#     if not buyer or not seller:
-#         Error.server_error()
-#
-#     offer = session.query(Offer.price).filter(Offer.id_advert == advert.id,
-#                                               Offer.id_buyer == id_user,
-#                                               Offer.is_accepted == True).first()
-#     if offer:
-#         price = offer.price
-#     else:
-#         price = advert.price
-#
-#     try:
-#
-#         setattr(buyer, 'balance', buyer.balance - price)
-#         setattr(seller, 'balance', seller.balance + price)
-#
-#     except Exception:
-#         session.rollback()
-#         return Error.server_error()
-# return '', 204
