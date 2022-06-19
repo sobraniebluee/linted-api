dee6cf404689b41159dce781543a23c07a1a1818
sobraniebluee <dimf29@gmail.com>
2022-06-19 16:58:38 +0200

    Update

--- main/middleware/error.py
+++ main/middleware/error.py
@@ -1,10 +1,10 @@
 class Error:
     @classmethod
-    def server_error(cls, msg="Server Error!"):
+    def server_error(cls, msg="Server error"):
         return {'message': str(msg)}, 500
 
     @classmethod
-    def error_not_found(cls, msg='Not Found!', status_code=404):
+    def error_not_found(cls, msg='Not found', status_code=404):
         return {'message': msg}, status_code
 
     @classmethod
